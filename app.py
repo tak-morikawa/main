ea01c674e2f7217ec5e67a9bac97305c1a3634e9
tak-morikawa <t_morikawa@chuoss.co.jp>
2025-12-03 22:45:39 +0900

    画像読み込み

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage
 )
 
 app = Flask(__name__)
